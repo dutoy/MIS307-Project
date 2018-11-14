cfab1249ebf0197406f257f0dd0f8736a8d861e0
Dannyalanis8 <44850839+Dannyalanis8@users.noreply.github.com>
2018-11-14 16:11:16 -0600

    Add files via upload

---
+++ TransactionInfo.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author danielalanis
+ */
+public class TransactionInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void CustomerName () {
+       
+        // Danny wrote code on 11/7/18 (look at progess report for description of class)
+        Scanner in = new Scanner(System.in);
+        
+        String name;
+        Double phone;
+        String email;
+        
+        System.out.println("Enter Customer Name:");
+        name = in.next();
+        
+        System.out.println("Enter Customer Phone #: ");
+        phone = in.nextDouble();
+        
+        System.out.println("Enter Customer Email: ");
+        email = in.next();
+    }
+    public static double SalesAmount(){
+        return MIS307project.double;
+}
+
+    
+}
+
