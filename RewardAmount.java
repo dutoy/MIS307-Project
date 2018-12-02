1a503ae6ae55c07ea397793d0d899b8f2a69df18
Isa Othman <45052983+ninsegando@users.noreply.github.com>
2018-12-02 14:30:12 -0600

    Add files via upload

---
+++ RewardAmount.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isaum
+ */
+public class RewardAmount {
+    
+    Customer customer = new Customer();
+    
+    public static void add(___) {
+        
+    }
+    
+    public static void remove(___) {
+        
+    }
+    
+    public static void update(__) {
+        
+    }
+    
+}
