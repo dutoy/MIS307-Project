e9c4069c67eeef30fb1f0edc4a5bb08db22b2c1a
DMarquis29 <44850888+DMarquis29@users.noreply.github.com>
2018-12-02 19:57:15 -0600

    Add files via upload

--- Item.java
+++ Item.java
@@ -3,11 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package pkgfinal.project;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 /**
  *
- * @author isaum
+ * @author Duncan
  */
 public class Item {
+    public static void main(String[] args) throws Exception{
+        stat.execute("CREATE TABLE Items (Item CHAR(25), Description VARCHAR(100)");
+        stat.execute("INSERT INTO Items VALUES('Iced Coffee', 'A chilled coffee.',");
+        stat.execute("INSERT INTO Items VALUES('Iced Coffee with Milk', 'A chilled coffee with milk.',");
+        stat.execute("INSERT INTO Items VALUES('Caffe Vanilla Frappuccino', 'A frosty frappccino with vanilla.',");
+        stat.execute("INSERT INTO Items VALUES('Caramel Frappuccino', 'An item in the coffee shop.',");
+        
+    }
+    
     
 }
